47b26f1c48904902aa3b337a014c985e0bd6958c
Ivan Bukrieiev <plag.ikar@gmail.com>
2024-11-19 00:28:14 +0100

    fix(GastroSOS): HashRouter

--- src/GastroSOS.tsx
+++ src/GastroSOS.tsx
@@ -8,14 +8,14 @@ import App from "./App";
 const GastroSos: React.FC = () => {
 	return (
 		// @ts-ignore
-		<BrowserRouter basename={import.meta.env.BASE_URL}
-		               future={{v7_startTransition: true, v7_relativeSplatPath: true}}>
-		{/*<HashRouter> /!* HashRouter создаёт хеш приложения, что позволяет разместить на gh-pages *!/*/}
+		// <BrowserRouter basename={import.meta.env.BASE_URL}
+		//                future={{v7_startTransition: true, v7_relativeSplatPath: true}}>
+		<HashRouter> {/* HashRouter создаёт хеш приложения, что позволяет разместить на gh-pages */}
 			<Provider store={store}>
 				<App/>
 			</Provider>
-		{/*</HashRouter>*/}
-		</BrowserRouter>
+		</HashRouter>
+		// </BrowserRouter>
 	);
 };
 
