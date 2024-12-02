776a761e1f98d0bace2dddedb8a928d0824f01d5
Ivan Bukrieiev <plag.ikar@gmail.com>
2024-12-03 00:54:23 +0100

    fix: ant

--- src/components/main/plus/PlusContainer.tsx
+++ src/components/main/plus/PlusContainer.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import style from '../../../styles/main/Plus.module.css'
-import {Button} from "antd";
 
 const PlusContainer: React.FC = () => {
 return (
 		<div className={style.plus}>
-			<Button type={"primary"}>OK</Button>
 		</div>
 	);
 };
